0382b3e617cd41778b7e912954b7f5910487c65c
Joseph Lawson <joe@joekiller.com>
2022-01-18 09:23:25 -0500

    fix meta for updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 1.0.3
+
+Fixed:
+
+* Meta for updates
+
 ## 1.0.2
 
 Fixed:
--- Joe's Classifieds Scanner.user.js
+++ Joe's Classifieds Scanner.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Joe's Backpack.tf Classifieds Scanner/Cacher
 // @namespace    https://joekiller.com
-// @version      1.0.2
+// @version      1.0.3
 // @description  Scans through backpack.tf classifieds pages with ctrl+leftarrow. When scanning stops a list is created sorted by listing date ascending. Ctrl + down arrow to force stop.
 // @author       Joseph Lawson
 // @match        *backpack.tf/classifieds*
-// @downloadURL  https://github.com/joekiller/joes-classifies-scanner/edit/main/Joe's%20Classifieds%20Scanner.user.js
-// @updateURL    https://github.com/joekiller/joes-classifies-scanner/edit/main/Joe's%20Classifieds%20Scanner.meta.js
+// @downloadURL  https://github.com/joekiller/joes-classifieds-scanner/raw/main/Joe's%20Classifieds%20Scanner.user.js
+// @updateURL    https://github.com/joekiller/joes-classifieds-scanner/raw/main/Joe's%20Classifieds%20Scanner.meta.js
 // ==/UserScript==
 let searchDelay = 0;
 
