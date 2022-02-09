66bc40a72c7a6873cd2e3f759710fea957ee6e25
Joseph Lawson <joe@joekiller.com>
2022-02-09 08:56:55 -0500

    tighter data and some filter options

--- merge-results.js
+++ merge-results.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require("path");
+const Steam64 = 76561198118254940;
 
 let paths;
 try {
@@ -15,10 +16,29 @@ try {
 
 
 async function mergeEm() {
+    const allowedKeys = [
+        'title',
+        'data-listing_account_id',
+        'data-listing_comment',
+        'data-listing_offers_url',
+        'data-listing_price',
+        'data-name',
+        'data-original_id',
+        'data-spell_1',
+        'data-spell_2',
+    ];
     const entries = {};
-    (await Promise.all(paths.flatMap(async (p) => JSON.parse(await fs.promises.readFile(p, {encoding: 'utf-8'}))))).flat().map(entry => entries[entry['id']] = entry['data']);
+    (await Promise.all(paths.flatMap(async (p) => JSON.parse(await fs.promises.readFile(p, { encoding: 'utf-8' }))))).flat().map(entry => {
+        const newData = {};
+        Object.entries(entry['data'])
+            .filter(([key,]) => allowedKeys.includes(key))
+            .forEach(([key, value]) => {
+                newData[key] = key === 'data-listing_account_id' ? (Number.parseInt(value) + Steam64).toString() : value;
+            });
+        entries[entry['id']] = newData;
+    });
     let dumpSorted = [];
-    Object.entries(entries).forEach(([key, value]) => dumpSorted.push({'id': key, 'data': value}));
+    Object.entries(entries).forEach(([key, value]) => dumpSorted.push({ 'id': key, 'data': value }));
     return dumpSorted;
 }
 
@@ -26,7 +46,7 @@ async function mergeEm() {
 async function sortEntries(entries, cmp, ascending) {
     const all = Array.from(entries);
     all.sort(cmp);
-    if(!ascending) {
+    if (!ascending) {
         all.reverse();
     }
     return all;
@@ -62,14 +82,23 @@ const sortFns = {
 const useAscending = true;
 
 const sortBy = 'originalId';
-const filters = {
-    'originalId': (entries) => entries.filter(entry => originalId <= Number.parseInt(entry['data']['data-original_id']) && "1095120776" !== entry['data']['data-listing_account_id'])
+const filterFn = {
+    'originalId': (entries) => entries.filter(entry =>
+        entry['data'].hasOwnProperty("data-spell_1")
+        && originalId <= Number.parseInt(entry['data']['data-original_id'])
+    ),
+    'excludes': (entries) => 
+        entries.filter(entry => 
+            exclusions.every(exclusion => 
+                (Number.parseInt(exclusion) + Steam64).toString() !== entry['data']['data-listing_account_id']))
 }
-const filter = 'originalId'; // or a key of filters ie 'originalId'
+const filters = []; // or a key of filters ie 'originalId' or 'excludes'
+// const filters = null; // or a key of filters ie 'originalId'
 const originalId = 3251762665;
+const exclusions = [];  // short steam id (partner id)
 
 mergeEm()
     .then(entries => sortEntries(entries, sortFns[sortBy], useAscending))
-    .then(entries => filter ? filters[filter](entries) : entries)
-    .then(entries => saveEntries(entries,  `merged-${[sortBy, useAscending?'asc':'dsc', filter].filter(e => null != e).join('-')}-${new Date().toISOString()}.json`))
+    .then(entries => filters ? filters.reduce((input, current) => filterFn[current](input), entries) : entries)
+    .then(entries => saveEntries(entries, `merged-${[sortBy, useAscending ? 'asc' : 'dsc', filters && filters.length > 0 ? filters.join('-') : null].filter(e => null != e).join('-')}-${new Date().toISOString()}.json`))
     .catch((e) => console.error(e.toString()));
